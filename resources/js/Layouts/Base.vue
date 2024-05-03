cb9134dbbab27bd360b25a05d13e61d473032d8b
Wortelbier <ivarschreutelkamp@gmail.com>
2024-05-03 12:51:00 +0200

    Feature: Main look of the site

--- resources/js/Layouts/Base.vue
+++ resources/js/Layouts/Base.vue
@@ -1,33 +1,48 @@
 <script setup lang="ts">
 import { Link } from '@inertiajs/vue3';
 import Footer from '@/Components/Footer.vue';
+import { defineExpose } from 'vue';
+
+const asset = (path: any) => `${import.meta.env.VITE_APP_URL}/storage/${path}`;
+
+defineExpose({ asset });
 </script>
 
 <template>
     <div class="w-screen h-screen bg-red-900 flex items-center justify-center">
         <div class="w-5/6 h-5/6 base-red">
-            <div class="text-center text-xl">
-                <div>Chinees Indische Specialiteiten</div>
-                <div>De Gouden Draak</div>
-                <nav>
-                    <div class="text-white flex justify-center items-center">
+            <div class="flex justify-between items-center">
+                <img :src="asset('pictures/dragon-large.png')" alt="Dragon" class="big-dragon m-8">
+                <div class="flex flex-col">
+                    <div class="text-center text-xl">
+                        <div class="main-text-sub m-8">
+                            Chinees Indische Specialiteiten
+                        </div>
+                        <div class="main-text m-8">
+                            De Gouden Draak
+                        </div>
+                    </div>
+                    <nav>
+                        <div class="text-white flex justify-center items-center">
                             <Link class="menu-button"
                             :href="route('menu')"
                             >
                             Menukaart
-                        </Link>
-                        <Link class="menu-button"
-                            :href="route('news')"
-                            >
-                            Nieuws
-                        </Link>
-                        <Link class="menu-button"
-                            :href="route('contact')"
-                            >
-                            Contact
-                        </Link>
-                    </div>
-                </nav>
+                            </Link>
+                            <Link class="menu-button"
+                                :href="route('news')"
+                                >
+                                Nieuws
+                            </Link>
+                            <Link class="menu-button"
+                                :href="route('contact')"
+                                >
+                                Contact
+                            </Link>
+                        </div>
+                    </nav>
+                </div>
+                <img :src="asset('pictures/dragon-large.png')" alt="Dragon" class="big-dragon inverted m-8">
             </div>
             <slot />
             <Footer />
