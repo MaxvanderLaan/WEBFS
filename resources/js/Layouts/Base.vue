ad82d96e156228a8d1b012c71c183f37858e1eb2
Wortelbier <ivarschreutelkamp@gmail.com>
2024-05-03 14:46:22 +0200

    Feature: scrolling text

--- resources/js/Layouts/Base.vue
+++ resources/js/Layouts/Base.vue
@@ -10,15 +10,38 @@ defineExpose({ asset });
 
 <template>
     <div class="w-screen h-screen bg-red-900 flex items-center justify-center">
-        <div class="w-5/6 h-5/6 base-red">
-            <div class="flex justify-between items-center">
-                <img :src="asset('pictures/dragon-large.png')" alt="Dragon" class="big-dragon m-8">
+        <div class="w-11/12 h-5/6 base-red">
+            <div class="flex justify-between">
+                <div class="flex items-center">
+                    <img :src="asset('pictures/dragon-small.png')" alt="Dragon" class="small-dragon">
+                    <div class="chinese-text">
+                        De Gouden Draak
+                    </div>
+                    <img :src="asset('pictures/dragon-small.png')" alt="Dragon" class="small-dragon inverted">
+                </div>
+
+                <div class="scroll-container flex">
+                    <div class="scroll-text">
+                        Welkom bij De Gouden draak. Klik op deze tekst om de aanbiedingen van deze week te zien!
+                    </div>
+                </div>
+
+                <div class="flex items-center">
+                    <img :src="asset('pictures/dragon-small.png')" alt="Dragon" class="small-dragon">
+                    <div class="chinese-text">
+                        De Gouden Draak
+                    </div>
+                    <img :src="asset('pictures/dragon-small.png')" alt="Dragon" class="small-dragon inverted">
+                </div>
+            </div>
+            <div class="flex flex-wrap justify-center items-center">
+                <img :src="asset('pictures/dragon-large.png')" alt="Dragon" class="big-dragon m-4">
                 <div class="flex flex-col">
                     <div class="text-center text-xl">
-                        <div class="main-text-sub m-8">
+                        <div class="main-text-sub m-4">
                             Chinees Indische Specialiteiten
                         </div>
-                        <div class="main-text m-8">
+                        <div class="main-text m-4">
                             De Gouden Draak
                         </div>
                     </div>
@@ -42,7 +65,7 @@ defineExpose({ asset });
                         </div>
                     </nav>
                 </div>
-                <img :src="asset('pictures/dragon-large.png')" alt="Dragon" class="big-dragon inverted m-8">
+                <img :src="asset('pictures/dragon-large.png')" alt="Dragon" class="big-dragon inverted m-4">
             </div>
             <slot />
             <Footer />
