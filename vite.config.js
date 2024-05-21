0b209ff3e61466da6b456bb337df7ca2b3ff7510
Wortelbier <ivarschreutelkamp@gmail.com>
2024-05-22 00:00:54 +0200

    Revert "Merge pull request #7 from MaxvanderLaan/feat/uc-20"
    This reverts commit 2480b768d46a07eaddacdcbc544a55c84e33c991, reversing
    changes made to c297be3b50c27e7cbbb114eb57b6106f8823598f.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-import i18n from 'laravel-vue-i18n/vite'; 
 
 export default defineConfig({
     plugins: [
@@ -17,6 +16,5 @@ export default defineConfig({
                 },
             },
         }),
-        i18n() 
     ],
 });
