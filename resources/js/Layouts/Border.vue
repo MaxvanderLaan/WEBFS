ae533c27cd79d888f7c479c01107eda8b8d99345
Wortelbier <ivarschreutelkamp@gmail.com>
2024-05-23 23:55:01 +0200

    My Fix so Hot

--- resources/js/Layouts/Border.vue
+++ resources/js/Layouts/Border.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-    <div class="w-screen h-screen bg-red-900 flex items-center justify-center">
+    <div class="w-full h-full bg-red-900 flex items-center justify-center">
     <table id="main_table" style="padding:5px;border-collapse: collapse">
 			<tr style="height:7px;background-color:red">
 				<td colspan="9">
