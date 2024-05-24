c1d60aaa2f726b09ce8b63abb5b9c111c4c4d8c4
AvansIvar <igm.schreutelkamp@student.avans.nl>
2024-05-24 13:16:49 +0200

    Feat: UC-14 done

--- resources/js/Pages/Menus/Menu.vue
+++ resources/js/Pages/Menus/Menu.vue
@@ -51,16 +51,33 @@ const props = defineProps({
 <template>
     <Base>
         <Head title="Menu" />
-        <div class="p-4 flex flex-col bg-yellow-200">
-            <div v-for="(group, mealType) in groupedMenus" class="flex flex-col mb-4">
+        <div class="p-4 flex flex-wrap justify-center bg-yellow-200">
+            <div v-for="(group, mealType) in groupedMenus" class="flex flex-col m-6">
                 <h2 class="text-lg font-bold mb-2">{{ mealType }}</h2>
-                <div v-for="menu in group" :key="menu.id" class="flex justify-center text-black text-sm">
-                    <div class="mr-4 flex flex-row space-x-1">
-                        <p>Menu Number: {{ menu.number }} </p>
-                        <p>Addition: {{ menu.addition }}</p>
-                        <p>Price: {{ menu.price }}</p>
-                        <div v-for="offer in menu.menu_offers" :key="offer.id">
-                            <p class="text-green-500">Discounted Price: {{ (Math.round(menu.price * (1 - offer.discount / 100) * 100) / 100).toFixed(2) }}</p>
+                <div class="flex flex-row space-x-4">
+                    <div class="flex flex-col">
+                        <div v-for="menu in group" :key="menu.id" class="flex justify-between text-black text-sm">
+                            <div class="flex flex-col space-x-4 overflow-hidden">
+                                <p>{{ menu.number }}. {{ menu.addition }}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex flex-col">
+                        <div v-for="menu in group" :key="menu.id" class="flex justify-between text-black text-sm">
+                            <div class="flex flex-col space-x-4 overflow-hidden">
+                                <p>{{ menu.name }}.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex flex-col">
+                        <div v-for="menu in group" :key="menu.id" class="flex justify-between text-black text-sm">
+                            <div class="flex flex-row space-x-1 overflow-hidden">
+                                <p v-if="menu.menu_offers.length > 0"><span class="line-through text-red-500">€ {{ menu.price }}</span></p>
+                                <p v-else>€ {{ menu.price }}</p>
+                                <div v-for="offer in menu.menu_offers" :key="offer.id" class="flex justify-end">
+                                    <p class="text-green-500">{{ (Math.round(menu.price * (1 - offer.discount / 100) * 100) / 100).toFixed(2) }}</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
