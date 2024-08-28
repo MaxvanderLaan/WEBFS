31dd9aec8adb94c72fee948f75cf6decf7d6e873
Wortelbier <ivarschreutelkamp@gmail.com>
2024-08-28 13:04:18 +0200

    Max....

--- resources/js/Layouts/Tablet.vue
+++ resources/js/Layouts/Tablet.vue
@@ -1,9 +1,6 @@
 <script setup lang="ts">
-import { Link } from '@inertiajs/vue3';
-import Border from '@/Layouts/Border.vue';
-import Footer from '@/Components/Footer.vue';
-import LocaleSwitcher from '@/Components/LocaleSwitcher.vue';
 import { defineExpose } from 'vue';
+import Base from './Base.vue';
 
 const asset = (path: any) => `/storage/${path}`;
 
@@ -11,34 +8,7 @@ defineExpose({ asset });
 </script>
 
 <template>
-	<Border>
-		<div class="base-red">
-        <LocaleSwitcher />
-            <div class="flex justify-between">
-                <div class="flex items-center">
-                    <img :src="asset('pictures/dragon-small.png')" alt="Dragon" class="small-dragon">
-                    <div class="chinese-text">
-                        {{ $t('messages.The Golden Dragon') }}
-                    </div>
-                        <img :src="asset('pictures/dragon-small.png')" alt="Dragon" class="small-dragon inverted">
-                    </div>
-
-                    <div class="scroll-container flex">
-                        <div class="scroll-text">
-                       {{ $t('messages.Welcome to the golden dragon, Tablet edition!') }}
-                        </div>
-                    </div>
-
-                    <div class="flex items-center">
-                        <img :src="asset('pictures/dragon-small.png')" alt="Dragon" class="small-dragon">
-                        <div class="chinese-text">
-                           {{ $t('messages.The Golden Dragon') }}
-                        </div>
-                        <img :src="asset('pictures/dragon-small.png')" alt="Dragon" class="small-dragon inverted">
-                    </div>
-                </div>
-                <slot />
-                <Footer />
-		</div>
-	</Border>
+	<Base>
+        <slot />
+	</Base>
 </template>
